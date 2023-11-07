3a084ecfab22c6581f90c4d808d3b0cae4f65760
Jiwoong Heo <jiwoong.heo@gmail.com>
2023-11-07 16:42:51 +0900

    Toggleterm

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,11 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm<cr>", { desc = "Toggle Terminal" })
+vim.keymap.set(
+  "n",
+  "<leader>fT",
+  "<cmd>ToggleTerm direction=vertical size=80<cr>",
+  { desc = "Toggle Terminal (Vertical)" }
+)
